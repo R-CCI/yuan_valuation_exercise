ebe8854958ef85fef59a49904e8b006423186b34
R-CCI <ymejia@cci.com.do>
2025-12-12 13:36:41 -0400

    Update Valuation.py

--- Valuation.py
+++ Valuation.py
@@ -1039,11 +1039,11 @@ with tab2:
         revenue_projections.append(revenue)
         ebitda_projections.append(ebitda)
         fcf_projections.append(fcf)
-        pv_fcf = [fcf_projections[i]/(1+wacc)**(i+1) for  i in range(5)]
-        pv_tv = ((fcf_projections[-1]*(1+tgr))/(wacc-tgr))/((1+wacc)**5)
-        ev_base = pv_fcf.sum() + pv_tv
-        st.write(f'Valor Presente de los Flujos: {pv_fcf.sum():,.2f}%')
-        st.write(f'Valor Presente del Valor Terminal: {pv_tv.sum():,.2f}%')
+    pv_fcf = [fcf_projections[i]/(1+wacc)**(i+1) for  i in range(5)]
+    pv_tv = ((fcf_projections[-1]*(1+tgr))/(wacc-tgr))/((1+wacc)**5)
+    ev_base = pv_fcf.sum() + pv_tv
+    st.write(f'Valor Presente de los Flujos: {pv_fcf.sum():,.2f}%')
+    st.write(f'Valor Presente del Valor Terminal: {pv_tv.sum():,.2f}%')
     
     # Display financial projections table
     st.markdown("#### 📊 5-Year Financial Projections")
