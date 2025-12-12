7db302cde4215944815660457d7dc68db0587d0a
R-CCI <ymejia@cci.com.do>
2025-12-12 14:35:30 -0400

    Update Valuation.py

--- Valuation.py
+++ Valuation.py
@@ -701,8 +701,6 @@ with st.sidebar:
     #last_price = info['regularMarketPreviousClose']
     total_equity = last_price * sharesOutstanding
     balance, income, cashflow = res['balance'].T, res['income'].T , res['cashflow'].T 
-    st.dataframe(income)
-    st.dataframe(balance)
     debt_long = balance.loc['Long Term Debt And Capital Lease Obligation'].iloc[0]
     st.write(f'Deuda: {debt_long:,.2f}')
     st.write(f'Equity: {total_equity:,.2f}')
@@ -1618,7 +1616,7 @@ if run_monte_carlo and simulation_results and len(simulation_results) > 100:
             wacc_range_viz = np.linspace(wacc_min * 100, wacc_max * 100, 10)
             terminal_range_viz = np.linspace(terminal_min * 100, terminal_max * 100, 10)
             wacc_plane, terminal_plane = np.meshgrid(wacc_range_viz, terminal_range_viz)
-            price_plane = np.full_like(wacc_plane, current_market_price)
+            price_plane = np.full_like(wacc_plane, current_market_price*2)
             
             fig_3d_scatter.add_trace(go.Surface(
                 x=wacc_plane,
